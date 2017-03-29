5eb353990c8d0a29d22a0b7ba4e4e9b56b9cf300
Victor Klimov <4klimov@gmail.com>
2017-03-30 02:23:08 +0300

    Refactor pep8_me.py and add test_pep8_me.py

--- lesson_1/homework/pep8_me.py
+++ lesson_1/homework/pep8_me.py
@@ -1,29 +1,33 @@
+import os
 import random
+import re
 import string
 
 
-def create_file(namef,dir,size):
-        if(size.isdigit()!=True):
-                if size.endswith('KB')==True:
-                        s1 = size.split('KB')
-                        size1 = int(s1[0])*1024
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-                if size.endswith('MB')==True:
-                        s1 = size.split('MB')
-                        size1 = int(s1[0])*1048567
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-                if size.endswith('GB') == True:
-                        s1 = size.split('GB')
-                        size1 = int(s1[0]) * 1073741824
-                        token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(size1))
-        else:
-                token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(int(size)))
+def create_file(file_name, directory, size):
+    regex = r"(\d+)([a-zA-Z]*)"
+    value, suffix = re.findall(regex, size)[0]
+    suffix_to_bytes = {
+        "": 1,
+        "B": 1,
+        "KB": 1024,
+        "MB": 1048576,
+        "GB": 1073741824,
+        "TB": 1099511627776
+    }
+    size_in_bytes = int(value) * suffix_to_bytes[suffix.upper()]
+    alphanumeric_characters = string.ascii_letters + string.digits
+    token = "".join(
+        random.choice(alphanumeric_characters) for _ in range(size_in_bytes)
+    )
+    file_path = os.path.join(directory, file_name)
+    file_handler = open(file_path, "w")
+    file_handler.write(token)
 
-        file = open(dir+namef,"w")
-        file.write(token)
 
-
-create_file("/test1.txt","E:",'10KB')
-create_file("/test2.txt","E:",'1024')
-create_file("/test11.txt","E:",'2MB')
-create_file("/test21.txt","E:",'1B')
+if __name__ == "__main__":
+    create_file("test1.txt", "/home/victor/", "2KB")
+    create_file("test2.txt", "/home/victor/", "10KB")
+    create_file("test3.txt", "/home/victor/", "1024")
+    create_file("test4.txt", "/home/victor/", "2MB")
+    create_file("test5.txt", "/home/victor/", "1B")
