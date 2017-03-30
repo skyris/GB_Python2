16562fcc8db14749482857c20f90d46ed8a5ee1c
Victor Klimov <4klimov@gmail.com>
2017-03-31 01:15:15 +0300

    Add function 'concat_files'

--- lesson_1/homework/concatenate_files.py
+++ lesson_1/homework/concatenate_files.py
@@ -1,4 +1,6 @@
 from functools import partial
+from hashlib import md5
+import os
 
 
 def divide_file(path, size_of_piece):
@@ -11,5 +13,23 @@ def divide_file(path, size_of_piece):
         return piece_number
 
 
-if __name__ == "__main__":
-    print(divide_file("need_hashes.csv", 50))
+def concat_files(directory, result_name):
+    hash_to_file = {}
+    files_list = os.listdir(directory)
+    for file in files_list:
+        full_path = os.path.join(directory, file)
+        if file.endswith(".md5"):
+            all_hashes_handle = open(full_path)
+            ordered_hashes = map(lambda x: x.rstrip("\n"), all_hashes_handle)
+        else:
+            with open(full_path, "rb") as file_handle:
+                h = md5()
+                h.update(file_handle.read())
+                hash_sum = h.hexdigest()
+                hash_to_file[hash_sum] = full_path
+
+    with open(result_name, "wb") as write_handle:
+        for current_hash in ordered_hashes:
+            current_file_path = hash_to_file[current_hash]
+            with open(current_file_path, "rb") as read_handle:
+                write_handle.write(read_handle.read())
