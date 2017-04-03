a3ec72bdc1c7d904cf7087c0ab42caf2b81140f7
Victor Klimov <4klimov@gmail.com>
2017-04-03 03:18:04 +0300

    Little changes

--- lesson_2/homework/server.py
+++ lesson_2/homework/server.py
@@ -1,30 +1,28 @@
-
 # Сервер игры "Запоминалка"
 
 import socketserver
 import random
 
-class MemTCPHandler(socketserver.BaseRequestHandler):
 
+class MemTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).decode()
         print("Клиент {} сообщает {}".format(self.client_address[0], self.data))
 
         if self.data == "I_WANNA_PLAY":
-            nums = random.sample(range(1,100), 18) * 2
+            nums = random.sample(range(1, 100), 18) * 2
             random.shuffle(nums)
             s_nums = [str(n) for n in nums]
             s_nums = ';'.join(s_nums)
 
-            self.request.sendall(bytes('NUMS;'+s_nums, 'utf-8'))
+            self.request.sendall(bytes('NUMS;' + s_nums, 'utf-8'))
         else:
-            print('Неизвестный запрос')    
+            print('Неизвестный запрос')
 
-          
+
 HOST, PORT = 'localhost', 9999
 
-server = socketserver.TCPServer((HOST, PORT), MemTCPHandler)  
+server = socketserver.TCPServer((HOST, PORT), MemTCPHandler)
 print('Сервер игры запущен')
 
 server.serve_forever()
-
